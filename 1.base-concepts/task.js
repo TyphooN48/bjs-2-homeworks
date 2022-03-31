8d342d0e5b8c3860c96d6e8088c1cf0631bf4cae
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-03-31 18:22:18 +0300

    add 1 task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,11 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  const D = (b**2) - 4 * (a != 0 ? a : 1) * (c != 0 ? c : 1)
+  switch (true) {
+    case D < 0: return []
+    case D == 0: return [-b/(2*a)]
+    case D > 0: return [(-b + Math.sqrt(D) )/(2*a), (-b - Math.sqrt(D) )/(2*a)]
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
