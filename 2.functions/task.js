ea69de0e8f7be864246ea2f7ba88428d617afcd6
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-04-06 19:50:03 +0300

    add solve

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,52 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0], max = arr[0], sum = 0, avg;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max) {
+      max = arr[i]
+    }
+    if(arr[i] < min) {
+      min = arr[i]
+    }
+    sum += arr[i]
+  }
+  avg = Number((sum / arr.length).toFixed(2))
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++)
+    sum += arr[i]
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for(let i = 0; i < arrOfArr.length; i++){
+    if (func(arrOfArr[i]) > max)
+      max = func(arrOfArr[i])
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity, max = -Infinity
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i]
+    }
+    if(arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  return (Math.abs(min - max))
 }
