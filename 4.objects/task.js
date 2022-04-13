abe2b87d85e1ec72bba1e42fb9d779b2eb2ed944
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-04-13 19:31:48 +0300

    Add solve

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name
+  this.gender = gender
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (mark === undefined || mark === null) {
+    return
+  }
+
+  if(this.marks === undefined) { 
+    this.marks = [mark]
+  } else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (mark1, mark2, ...marks ) {
+  this.addMark(mark1)
+  this.addMark(mark2)
+  for (mark of marks) { 
+    this.addMark(mark)
+  }
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((sum, value) => sum + value, 0) / this.marks.length
+}
+
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks
+  delete this.subject
+  this.excluded = reason
+}
