71d564b4ccf27917e7bce04edfd7841a8b76541d
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-04-27 14:16:08 +0300

    add solve

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,52 @@
+function parseCount(str) {
+	let num = Number.parseInt(str, 10)
+	if (Number.isNaN(num)) {
+		throw new Error('Невалидное значение')
+	} else {
+		return  num
+	}
+}
+
+function validateCount(str) {
+	try {
+		return parseCount(str)
+	} catch(err) {
+		return err
+	}
+}
+
+class Triangle {
+	constructor (a, b, c) {
+		this.a = a
+		this.b = b
+		this.c = c
+
+		if (((this.a + this.b) < this.c) || ((this.a + this.c) < this.b) || ((this.b + this.c) < this.a)) {
+			throw new Error("Треугольник с такими сторонами не существует")
+		}
+	}
+
+	getPerimeter() {
+		return this.a + this.b + this.c
+	}
+
+	getArea() {
+        let p = this.getPerimeter() / 2
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+	try {
+        return new Triangle(a, b, c);
+    } catch {
+        return {
+            getPerimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            getArea() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
