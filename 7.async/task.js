bfb43ffd4c260e15a0c552acea1b19ffc1983c24
Andrey Burlakov <43408988+TyphooN48@users.noreply.github.com>
2022-04-29 13:25:10 +0300

    add solve

---
+++ 7.async/task.js
@@ -0,0 +1,94 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [],
+        this.timerId = null
+    }
+
+    addClock(time, callback, id) {
+        if (!id) {
+            throw new Error('Panic!!!')
+        }
+        if (this.alarmCollection.find(item => item.id === id)) {
+            console.error('warning')
+        } else {
+            this.alarmCollection.push({
+                id: id,
+                time: time,
+                callback: callback,
+            })
+        }
+    }
+
+    removeClock(id) {
+        let start = this.alarmCollection.length
+        this.alarmCollection = this.alarmCollection.filter(item => item.id !== id)
+
+        let finish = this.alarmCollection.length
+
+        return start > finish ? true : false
+    }
+
+    getCurrentFormattedTime() {
+        let nowTime = new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+        })
+
+        return nowTime
+    }
+
+    start() {
+        function checkClock(array, currunetTime) {
+            array.forEach(item => {
+                if (item.time == currunetTime) {
+                    item.callback()
+                }
+            })
+        }
+        if (this.timerId === null) {
+            this.timerId = setInterval(() => checkClock(this.alarmCollection, this.getCurrentFormattedTime()), 1000)
+        }
+
+    }
+
+    stop() {
+        if (this.timerId !== null) {
+            clearInterval(this.timerId)
+            this.timerId = null
+        }
+    }
+
+    printAlarms() {
+        this.alarmCollection.forEach(alarm => {
+            console.log(`Будильник № ${alarm.id} заведен на ${alarm.time}`)
+        })
+
+    }
+
+    clearAlarms() {
+        this.stop()
+        this.alarmCollection = []
+    }
+}
+
+function testCase() {
+    let phoheAlarm = new AlarmClock()
+
+    phoheAlarm.addClock("13:21", () => console.log('вставай'), 1)
+    phoheAlarm.addClock("13:22", () => {console.log('вставай'); phoheAlarm.removeClock(2)}, 2)
+    /*phoheAlarm.addClock("13:16", () => console.log('вставай'))*/
+
+    phoheAlarm.addClock("13:23", () => {
+        console.log('123');
+        phoheAlarm.clearAlarms();
+        phoheAlarm.printAlarms();
+    }, 3)
+
+    phoheAlarm.addClock("13:24", () => console.log('вставай'), 1)
+
+    phoheAlarm.printAlarms()
+
+    phoheAlarm.start()
+}
+
+testCase()
